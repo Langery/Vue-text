de6ef39be92f9f742cfc0c429b92cad3bb4c41ca
Lahy <1245066197@qq.com>
2018-01-17 20:03:33 +0800

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import one from '../components/one.vue'
+import newpage from '@/components/new'
+import other from '@/components/inter'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },{
+      path: '/one',
+      component: one
+    },{
+      path: '/new page',
+      name: 'new page',
+      component: newpage
+    },{
+      path: '/new/inter',
+      component: other
+    }
+  ]
+})
