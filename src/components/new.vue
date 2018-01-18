1f0e52d260542665697e80103f6f5b486b1e3ef0
Lahy <1245066197@qq.com>
2018-01-18 19:38:07 +0800

    Update new.vue

--- src/components/new.vue
+++ src/components/new.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="new">
-      <h1>你好</h1>
+      <h1>你好,风景</h1>
       <a href="/#/new/inter" class="newpage">这是新的一页</a>
+      <img src="../images/2.jpg" alt="">
   </div>
 </template>
 
@@ -16,7 +17,12 @@ h1{
     font-weight: normal;
 }
 .newpage{
+    display: inline-block;
     text-decoration: none;
     color: lightseagreen;
+    margin-bottom: 20px;
+}
+img{
+    margin-bottom: 20px;
 }
 </style>
