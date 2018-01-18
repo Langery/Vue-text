6bcbcd326b2516e9cfed7ed5140c4bb09fcd6bc2
Lahy <1245066197@qq.com>
2018-01-18 08:07:40 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <br>
+    <br>
+    <div id="view">
+        <router-link class="title" to="/">Go to view1</router-link>
+        <router-link class="title" to="/one">Go to view2</router-link>
+        <router-link class="title" to="/new page">Go to new page</router-link>
+        <router-view></router-view>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img{
+  display: block;
+  margin: 0 auto;
+}
+#view{
+  text-align: center;
+}
+.title{
+  margin: 0 5%;
+  text-decoration: none;
+  color:lightgreen;
+}
+</style>
