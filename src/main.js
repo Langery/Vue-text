6f12cb62bc1e4c25595cdbb6e32cb6eef89b3633
Lahy <1245066197@qq.com>
2018-01-19 13:48:05 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue' // 引入vue模块
+import App from './App' // 引入vue组件
+import router from './router' // 引入路由配置文件
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // 关闭生产模式下给出的提示
+
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  el: '#app', // 挂载id,实例下所有内容都会在index.html中的#app下显示
+  router, // 注入路由配置
+  template: '<App/>', // 配置根模板
+  components: { App } // 注入组件
 })
