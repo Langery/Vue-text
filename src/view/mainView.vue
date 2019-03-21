aaad4e6f37ac51cdd216b8acb95be15fbac9a61a
Langery <1245066197@qq.com>
2019-03-21 23:08:47 +0800

    write a new page

---
+++ src/view/mainView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mainView">
+    <h1>彩蛋</h1>
+    <p>点击一次和双击</p>
+    <button @click="first()" @dblclick="second()">点击一下</button>
+    <span>{{say}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      say: ''
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    first () {
+      const person = {
+        name: 'Story',
+        age: '24',
+        like: 'guitar',
+        say: '第一个彩蛋'
+      }
+      this.say = person.say
+    },
+    second () {
+      this.say = ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.mainView{}
+</style>
