81e081170690c164daede95f06d50434d671c6ef
Langery <1245066197@qq.com>
2019-08-13 16:26:35 +0800

    add a new model

--- src/view/components/D3/radialDendrogram.vue
+++ src/view/components/D3/radialDendrogram.vue
@@ -71,6 +71,70 @@ export default {
         })
     },
     init (data) {
+      const tree = (data) => {
+        var width = 932
+        const root = d3.hierarchy(data)
+                        .sort((a, b) => {
+                          (a.height - b.height) || a.data.name.toString().localeCompare(b.data.name.toString())
+                        })
+        root.dx = 10
+        root.dy = width / (root.height + 1)
+        return d3.cluster().nodeSize([root.dx, root.dy])(root)
+      }
+      const root = tree(data)
+      let x0 = Infinity;
+      let x1 = -x0;
+      root.each(d => {
+        if (d.x > x1) x1 = d.x;
+        if (d.x < x0) x0 = d.x;
+      });
+
+      const svg = d3.select('svg')
+          .style("width", "100%")
+          .style("height", "600px");
+
+      const g = svg.append("g")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 10)
+          .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
+
+      const link = g.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
+        .selectAll("path")
+        .data(root.links())
+        .join("path")
+          .attr("d", d => `
+            M${d.target.y},${d.target.x}
+            C${d.source.y + root.dy / 2},${d.target.x}
+            ${d.source.y + root.dy / 2},${d.source.x}
+            ${d.source.y},${d.source.x}
+          `);
+
+      const node = g.append("g")
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-width", 3)
+          .selectAll("g")
+          .data(root.descendants().reverse())
+          .join("g")
+          .attr("transform", d => `translate(${d.y},${d.x})`);
+
+      node.append("circle")
+          .attr("fill", d => d.children ? "#555" : "#999")
+          .attr("r", 2.5);
+
+      node.append("text")
+          .attr("dy", "0.31em")
+          .attr("x", d => d.children ? -6 : 6)
+          .text(d => d.data.name)
+        .filter(d => d.children)
+          .attr("text-anchor", "end")
+        .clone(true).lower()
+          .attr("stroke", "white");
+    },
+    /* init (data) {
       console.log(data)
       // console.log(d3)
       // (a.height - b.height) || a.data.name.localeCompare(b.data.name)
@@ -127,7 +191,7 @@ export default {
           .filter(d => d.children)
           .clone(true).lower()
           .attr('stroke', 'white')
-    },
+    }, */
     // project (x, y) {
     //   var angle = (x - 90) / 180 * Math.PI, radius = y
     //   return [radius * Math.cos(angle), radius * Math.sin(angle)]
