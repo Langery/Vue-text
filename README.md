eee48277229f7ec73bbe4557c7b04817c304bca7
Langery <1245066197@qq.com>
2019-08-15 17:26:11 +0800

    build chord file

--- README.md
+++ README.md
@@ -71,6 +71,7 @@ npm install
   6. [D3 API Reference](https://github.com/d3/d3/blob/master/API.md)
   7. [svg给出两点坐标连一条曲线](https://www.jianshu.com/p/30f1ee0809d7)
   8. [D3 Cluster Dendrogram](https://observablehq.com/@awring/d3-cluster-dendrogram)
+  9. [【 D3.js 入门系列 --- 9.3 】 弦图的制作](http://blog.sina.com.cn/s/blog_663d9a1f0102w5gn.html)
 
 > 引入和使用
 
--- src/view/components/D3/chordDia.vue
+++ src/view/components/D3/chordDia.vue
@@ -18,8 +18,8 @@ export default {
     getSampleGraph () {
       const sendOther = {
         graphName: 'Knowledge',
-        limitNum: 5,
-        hopNum: 3,
+        limitNum: 3,
+        hopNum: 2,
         arrayV: [1]
       }
       intranetPost(multiExpand, sendOther)
@@ -29,15 +29,17 @@ export default {
         })
         .catch(reason => { console.log(reason) })
     },
-    init (data) {
+    init (data, keyName) {
+      console.log(keyName)
       // console.log(data)
       // var data = [
-      //   [11975,  5871, 8916, 2868],
-      //   [ 1951, 10048, 2060, 6171],
-      //   [ 8010, 16145, 8090, 8045],
-      //   [ 1013,   990,  940, 6907]
+      //   [11975,  5871, 8916, 2868, 2000],
+      //   [ 1951, 10048, 2060, 6171, 4512],
+      //   [ 8010, 16145, 8090, 8045, 1236],
+      //   [ 1013,   980,  194,  907, 8521],
+      //   [ 1013,  1990,  940, 6907, 8521]
       // ]
-      var data = data.slice(0, 5)
+      // var data = data.slice(0, 11)
       console.log(data)
 
       var height = 960
@@ -61,7 +63,7 @@ export default {
                         .selectAll("g")
                         .data(chords.groups)
                         .enter().append("g")
-      
+
       var color = d3.scaleOrdinal()
                     .domain(d3.range(data.length))
                     .range(["#000000", "#FFDD89", "#957244", "#F26223", '#F2CAC9', '#F4A83A', '#bbb5ac', '#f2e6ce', '#393733', '#bacf65', '#e2d849', '#c0c4c3', '#248067', '#737c7b', '#7cabb1', '#2e317c', '#815c94'])
@@ -78,35 +80,62 @@ export default {
         })
       }
       group.append("path")
-          .style("fill", d => { return color(d.index) } )
-          .style("stroke", d => { return d3.rgb(color(d.index)).darker() })
+          .attr("fill", d => { return color(d.index) } )
+          .attr("stroke", d => { return color(d.index) })
           .attr("d", arc)
       
       let r0 = Math.min(width, height) * .37
 
       group.append('text')
-          .each(d => {
-            console.log(d)
+          .each((d, i) => {
+            // console.log(d)
             d.angle = (d.startAngle + d.endAngle) / 2
+            d.name = keyName[i]
           })
-          .attr('dy', '.40em')
+          .attr('dy', '.35em')
           .attr('text-anchor', d => {
             return d.angle > Math.Pi ? 'end' : null
           })
           .attr('transform', d => {
-            return  "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
-              + "translate(" + (r0 + 20) + ")"
-              + (d.angle > Math.PI ? "rotate(180)" : "")
+            // return  "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+            //   + "translate(" + (r0 + 20) + ")"
+            //   + (d.angle > Math.PI ? "rotate(180)" : "")
+            var result =  "rotate(" +　(d.angle*180/Math.PI) + ")";
+            result += "translate(0," + -1 * (outerRadius + 10) + ")";
+            if (d.angle > Math.PI * 3 / 4 && d.angle < Math.PI * 5 / 4 )
+                result += "rotate(180)";
+            return result
           })
           .attr('stroke', '#000')
           .text(d => {
-            return d.index
+            return d.name
           })
-      // const groupTick = group.append("g")
-      //   .selectAll("g")
-      //   .data(d => groupTicks(d, 1e3))
-      //   .join("g")
-      //   .attr("transform", d => { `rotate(${d.angle * 180 / Math.PI - 90}) translate(${outerRadius},0` })
+
+      // group.selectAll('text')
+      //       .enter()
+      //       .append('text')
+      //       .each((d,i) => {
+      //         d.angle = (d.startAngle +　d.endAngle) / 2
+      //         d.name = keyName[i]
+      //       })
+      //       .attr("dy",".35em")
+      //       .attr('transform', (d) => {    // 平移属性
+      //           var result =  "rotate(" +　(d.angle*180/Math.PI) + ")";
+      //           result += "translate(0," + -1 * (outerRadius + 10) + ")";
+      //           if (d.angle > Math.PI * 3 / 4 && d.angle < Math.PI * 5 / 4 )
+      //               result += "rotate(180)";
+      //           return result;
+      //       })
+      //       .text((d) => {
+      //         console.log(d)
+      //         return d.name;
+      //       })
+
+      const groupTick = group.append("g")
+        .selectAll("g")
+        .data(d => groupTicks(d, 1e3))
+        .join("g")
+        .attr("transform", d => { `rotate(${d.angle * 180 / Math.PI - 90}) translate(${outerRadius},0` })
       
       // groupTick
       //   .filter(d => d.value % 5e3 === 0)
@@ -128,31 +157,83 @@ export default {
 
     },
     dataDeal (data) {
+      var keyName = []
+      data.nodes.forEach(item => { keyName.push(item.__key) })
+
+      var newLinks = []
+      data.links.forEach(item => {
+        var pushI = {
+          __s: item.__d,
+          __d: item.__s,
+          src: item.dst,
+          dst: item.src,
+          src_type: item.dst_type,
+          dst_type: item.src_type,
+          type: item.type,
+          __e: item.__e
+        }
+        newLinks.push(pushI)
+      })
+      console.log(newLinks)
+      var sendLinks = data.links.concat(newLinks)
+      console.log(sendLinks)
+
+      // .entries(data.links)
       let nested = d3.nest().key(d => d.__s)
-                      .entries(data.links)
-      // console.log(nested)
+                      .entries(sendLinks)
+      console.log(nested)
       let n = d3.max(nested, d => d3.max(d.values, x => x.__d)) + 1
       let m = nested.length
-      // console.log(n, m)
+      console.log(n, m)
       let matrix = []
+      let trueMatrix = []
+
       nested.forEach((x, i) => {
-        if (!matrix[+x.key % m]) matrix[+x.key % m] = Array.from({length: n}).fill(0)
+
+        if (!matrix[+x.key % m]) {
+          matrix[+x.key % m] = Array.from({length: n}).fill(0)
+        } else {
+          trueMatrix[+x.key % m] = matrix[+x.key % m]
+        }
+
         x.values.forEach((d, j) => {
-          // console.log(d)
-          // console.log(j)
-          matrix[d.__s % m][d.__d % n] = d.__e
+          matrix[d.__s % m][d.__d % n] = 1
         })
       })
+      // console.log(nested)
+      // console.log(matrix)
+      // console.log(trueMatrix)
+      var newMatrix = []
+      matrix.forEach(item => newMatrix.push(item))
+      trueMatrix.forEach(item => newMatrix.push(item))
+      // console.log(newMatrix)
       
-      var newMat = []
-      matrix.map(item => {
-        item = item.filter(Boolean)
-        newMat.push(item)
-      })
+      var max = newMatrix[0].length
+      for (var i = 0; i < newMatrix.length; i++) {
+        // console.log(newMatrix[i])
+        if (max < newMatrix[i].length) max = newMatrix[i].length
+      }
+      console.log(max)
 
-      // console.log(newMat)
+      if (newMatrix.length > max || newMatrix.length == max) {
+        max = newMatrix.length
+        newMatrix.forEach(item => {
+          if (item.length < max) {
+            var addN = max - item.length
+            for (var i = 0; i < addN; i++) item.push(0)
+          }
+        })
+      } else if (newMatrix.length < max) {
+        var lessN = max - newMatrix.length
+        var newArr = []
+        for (var j = 0; j < max; j++) newArr.push(0)
+        newMatrix.push(newArr)
+      }
+
+      console.log(newMatrix)
+
       // this.init(matrix)
-      this.init(newMat)
+      this.init(newMatrix, keyName)
     }
   }
 }
