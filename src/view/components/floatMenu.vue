04cd4d9d257269502a7a805ab48d7ff1898fda22
Langery <1245066197@qq.com>
2020-09-18 11:10:04 +0800

    merge

--- src/view/components/floatMenu.vue
+++ src/view/components/floatMenu.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="float-menu" ref="floatMenu">
     <!-- loop list -->
-    <img src="../../assets/logo.png" />
+    <div class="left-icon">
+      <img src="../../assets/logo.png" />
+      <!-- <slot></slot> -->
+    </div>
     <div class="right-infor">
       <Anchor show-ink>
         <AnchorLink href="#one" title="Basic Usage" />
@@ -34,15 +37,19 @@ export default {
   right: 0;
   top: 100px;
 }
-img{
-  background: #ccffcc;
-  border-radius: 5px;
+.left-icon{
+  display: inline-block;
   width: 50px;
   height: 50px;
-  display: inline-block;
-  position: absolute;
-  top: 0;
-  left: -50px;
+  background: #ccffcc;
+  position: relative;
+  right: 0px;
+  top: -125px;
+  border-radius: 20% 0 0 20%;
+}
+img{
+  width: 40px;
+  height: 40px;
 }
 .right-infor{
   display: inline-block;
@@ -51,8 +58,10 @@ img{
   -moz-transition: width 800ms; /* Firefox 4 */
   -webkit-transition: width 800ms; /* Safari and Chrome */
   -o-transition: width 800ms; /* Opera */
+  border: 2px #ccc solid;
+  text-align: start;
 }
-img:hover ~ .right-infor{
+.left-icon:hover ~ .right-infor{
   width: 150px;
 }
 .right-infor:hover{
